2856da45940d6c6d3c311693655fc13946e6dfbf
Paulo Cremon <cremonpaulo@gmail.com>
2022-08-24 11:26:12 -0300

    Commit semi finished

---
+++ HappyCity/Assets/Scripts/InitialHud.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialHud : MonoBehaviour
+{
+   public void Close()
+    {
+        Destroy(this.gameObject);
+    }
+}
